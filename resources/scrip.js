9cad7d0cfdf7c4ee03b76930c516787eedd37f63
Efrain Valenzuela <efrainvalenzuela@MacBook-Air-de-Efrain.local>
2025-09-25 10:21:11 -0700

    segundo commit

---
+++ resources/scrip.js
@@ -0,0 +1,25 @@
+// Evento que se dispara cuando se termina de cargar el DOM
+//DOM documnet objet model
+
+document.addEventListener("DOMContentLoaded", function(){
+    //declaramos una variable en javaScrip que representa el 
+    var btnOK = document.getElementById("btn-ok");
+
+
+    // declaramos un evento click a ese boton 
+    btnOK.addEventListener("click",function(){
+
+        //declaramos una variable que represnta el imput en html
+        var name = document.getElementById("input-name");
+
+        //mostramos alerta del navegador con el contenido del input
+        alert("hello world"   +   name.value);
+
+
+
+    })
+
+
+
+
+})
